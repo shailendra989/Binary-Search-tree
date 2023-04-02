f794437e7fb27be274892dba80c6c2edcb09ac4d
Shailendra kumar pal <palshailendrakumar123@gmail.com>
2023-04-03 00:02:51 +0530

    first l

---
+++ BinarySearchTree.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    //Creating BST using generics and IComparable to use CompareTo method in program
+    class BinarySearchTree<T> where T : IComparable<T>
+    {
+        int leftCount = 0, rightCount = 0;
+        bool result = false;
+        public T NodeData { get; set; }
+        public BinarySearchTree<T> leftTree { get; set; }
+        public BinarySearchTree<T> rightTree { get; set; }
+
+        public BinarySearchTree(T nodeData)
+        {
+            this.NodeData = nodeData;
+            this.rightTree = null;
+            this.leftTree = null;
+
+        }
+
+        //Method to check node and add node value to either left or right by comparing
+        public void Insert(T item)
+        {
+            T currentNodeValue = this.NodeData;
+            if ((currentNodeValue.CompareTo(item)) > 0)
+            {
+                if (this.leftTree == null)
+                {
+                    this.leftCount++;
+                    this.leftTree = new BinarySearchTree<T>(item);
+                }
+                else
+                {
+
+                    this.leftCount++;
+                    this.leftTree.Insert(item);
+                }
+            }
+            else
+            {
+                if (this.rightTree == null)
+                {
+                    this.rightCount++;
+                    this.rightTree = new BinarySearchTree<T>(item);
+                }
+                else
+                {
+                    this.rightCount++;
+                    this.rightTree.Insert(item);
+                }
+            }
+        }
+
+        //Displaying the data in the binary tree
+        public void Display()
+        {
+            if (this.leftTree != null)
+            {
+                this.leftTree.Display();
+            }
+            Console.WriteLine(this.NodeData.ToString());
+            if (this.rightTree != null)
+            {
+                this.rightTree.Display();
+            }
+        }
+
+        //Method to Get Size of the binary tree
+        public void GetSize()
+        {
+            Console.WriteLine("Size of Binary Tree " + " " + (1 + this.leftCount + this.rightCount));
+        }
+
+        //Method tto search the given value in the binary tree
+        public bool IfExists(T element, BinarySearchTree<T> node)
+        {
+            if (node == null)
+                return false;
+            if (node.NodeData.Equals(element))
+            {
+                Console.WriteLine("Found the element in BST" + " " + node.NodeData);
+                result = true;
+            }
+            else
+                Console.WriteLine("Current element is {0} in BST", node.NodeData);
+            if (element.CompareTo(node.NodeData) < 0)
+                IfExists(element, node.leftTree);
+            if (element.CompareTo(node.NodeData) > 0)
+                IfExists(element, node.rightTree);
+            return result;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿namespace BinarySearchTree
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Binary Search Tree Problem !!!");
+
+            BinarySearchTree<int> binaryTree = new BinarySearchTree<int>(56);
+            Console.WriteLine("\n1.Create Binary Search Tree \n2.Size of Binary Tree \n3.Search Binary Tree");
+            Console.WriteLine("Select the option from above");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine("Creating BST to insert and display node value");
+                    binaryTree.Insert(30);
+                    binaryTree.Insert(70);
+                    binaryTree.Display();
+                    break;
+                case 2:
+                    Console.WriteLine("Inserting more node and get size of binary tree ");
+                    binaryTree.Insert(30);
+                    binaryTree.Insert(70);
+                    binaryTree.Insert(22);
+                    binaryTree.Insert(44);
+                    binaryTree.Insert(60);
+                    binaryTree.Insert(95);
+                    binaryTree.Insert(11);
+                    binaryTree.Insert(65);
+                    binaryTree.Insert(3);
+                    binaryTree.Insert(16);
+                    binaryTree.Insert(63);
+                    binaryTree.Insert(67);
+                    binaryTree.Display();
+                    binaryTree.GetSize();
+                    break;
+                case 3:
+                    binaryTree.Insert(30);
+                    binaryTree.Insert(70);
+                    binaryTree.Insert(22);
+                    binaryTree.Insert(44);
+                    binaryTree.Insert(60);
+                    binaryTree.Insert(95);
+                    binaryTree.Insert(11);
+                    binaryTree.Insert(65);
+                    binaryTree.Insert(3);
+                    binaryTree.Insert(16);
+                    binaryTree.Insert(63);
+                    binaryTree.Insert(67);
+                    Console.WriteLine("Searching node is present or not in binary tree");
+                    bool result = binaryTree.IfExists(63, binaryTree);
+                    break;
+                default:
+                    Console.WriteLine("Please enter the correct option");
+                    break;
+            }
+        }
+    }
+}
